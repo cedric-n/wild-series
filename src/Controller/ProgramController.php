4d908a5e91866b2b4fbd5a4855fc44ea1257e3e7
cedric-n <cedric.ngondo@gmail.com>
2020-12-01 02:16:25 +0100

    finifh quest routeur

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,15 +6,32 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/programs/", name="program_")
+ */
 class ProgramController extends AbstractController
 {
 
     /**
-     * @Route("/programs/", name="program_index")
+     * @return Response
+     * @Route("", name="index")
      */
     public function index(): Response
     {
 
-        return $this->render('program/index.html.twig', ['website' => 'Wild Serie']);
+        return $this->render('program/index.html.twig', ['website' => 'Wild Series']);
     }
+
+    /**
+     * @param int $id
+     * @return Response
+     * @Route("{id}",methods={"GET"}, requirements={"id"="\d+"}, name="show")
+     */
+    public function show(int $id = 1): Response
+    {
+
+        return $this->render('program/show.html.twig', ['id' => $id]);
+
+    }
+
 }
