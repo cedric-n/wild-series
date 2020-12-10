6620e8845448c97f4d072664c3174d4b61aa639a
cedric-n <cedric.ngondo@gmail.com>
2020-12-11 00:04:36 +0100

    finish quest 11

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 lien quête 9: https://www.loom.com/share/b9db3475a34a4d0ebffd2b661aaecdcd
 lien quête 10: https://www.loom.com/share/c587a20fb9b64556908e09fbfdd8ab85
+lien quête 11: https://www.loom.com/share/1ff3e844696e40a29e623da2b58a3944
--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -4,10 +4,13 @@ namespace App\Controller;
 
 
 use App\Entity\Category;
+use App\Form\CategoryType;
 use App\Entity\Program;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
 
 /**
  * Class CategoryController
@@ -29,6 +32,36 @@ class CategoryController extends AbstractController
         return $this->render('category/index.html.twig',
             ['categories' => $categories]
         );
+    }
+
+    /**
+     * @param Request $resquest
+     * @return Response
+     * @Route("/new", name="new")
+     */
+    public function new(Request $resquest): Response
+    {
+
+        $category = new Category();
+
+        $form = $this->createForm(CategoryType::class, $category);
+
+        $form->handleRequest($resquest);
+
+        if ($form->isSubmitted()) {
+
+            $entityManager = $this->getDoctrine()->getManager();
+
+            $entityManager->persist($category);
+
+            $entityManager->flush();
+
+            return $this->redirectToRoute('category_index');
+        }
+
+        return $this->render('category/new.html.twig', [
+            "form" => $form->createView(),
+        ]);
     }
 
     /**
